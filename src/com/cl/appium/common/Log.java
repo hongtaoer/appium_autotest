d96de075d57d25b97ea78fdf4755d18977f31aae
hongtaoer <286754050@qq.com>
2016-07-20 20:58:12 +0800

    增加日志模块

---
+++ src/com/cl/appium/common/Log.java
@@ -0,0 +1,33 @@
+package com.cl.appium.common;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by cl on 7/20/16.
+ */
+public class Log {
+
+    static {
+
+        BasicConfigurator.configure();
+        String configLocation = System.getProperty("user.dir")+"/resource/log4j.properties";
+        System.out.println("========================================"+configLocation);
+        PropertyConfigurator.configure(configLocation);
+//        DOMConfigurator.configure("");
+    }
+
+    /**
+     * 根据类名获取日志对象
+     *
+     * @param clazz
+     * @return
+     */
+    public static Logger getLog(Class clazz) {
+
+        return LoggerFactory.getLogger(clazz);
+    }
+}
--- test/com/appium/test/ios/shipper/LoginTest.java
+++ test/com/appium/test/ios/shipper/LoginTest.java
@@ -1,32 +1,40 @@
 package com.appium.test.ios.shipper;
 
 
-
-import com.cl.appium.ios.shipper.order.CJPublishOrder;
+import com.cl.appium.common.Log;
 import com.cl.appium.ios.shipper.user.ShipperLogin;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by chenliu on 16/6/25.
  */
 public class LoginTest {
     private static IOSDriver<IOSElement> driver;
+    private static final Logger LOG = Log.getLog(LoginTest.class);
 
     // 启动app时 就相当于创建了一个session
-    @BeforeClass
+//    @BeforeClass
     public static void beforeClass() throws Exception {
-        driver =InitializeShipperIOSDriver.init();
+        driver = InitializeShipperIOSDriver.init();
     }
 
     @Test
-    public void login(){
+    public void login() {
         ShipperLogin.loginByPassword(driver);
     }
 
+
+    @Test
+    public void logTest() {
+        LOG.error("This is appium autotest error log");
+        LOG.info("This is appium autotest info log");
+    }
 
     @AfterClass
     public static void afterClass() {
@@ -34,4 +42,5 @@ public class LoginTest {
 //            driver.quit();
         }
     }
+
 }
